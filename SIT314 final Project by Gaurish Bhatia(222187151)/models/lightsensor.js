b378d42e4167e73decb685d9c374f310eeebda1a
gaurish-0001 <128626986+gaurish-0001@users.noreply.github.com>
2023-10-08 14:21:05 +1100

    Update lightsensor.js

--- SIT314 final Project by Gaurish Bhatia(222187151)/models/lightsensor.js
+++ SIT314 final Project by Gaurish Bhatia(222187151)/models/lightsensor.js
@@ -1,4 +1,11 @@
+// Name : Gaurish Bhatia
+// This is the schema for the sensor data to be published.
+
+
+// including the mongoose modules.
 const mongoose = require('mongoose');
+
+// defining the schema in the model as light sensor.
 module.exports = mongoose.model('light_sensor', new mongoose.Schema({
  id: Number,
  name: String,
