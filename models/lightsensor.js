07ec9c66d6158fe78bf51651a007863a4d5528af
gaurish-0001 <128626986+gaurish-0001@users.noreply.github.com>
2023-10-08 13:08:19 +1100

    Delete models directory

--- models/lightsensor.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-module.exports = mongoose.model('light_sensor', new mongoose.Schema({
- id: Number,
- name: String,
- address: String,
- time: Date,
- lightintensity : Number
-}));
